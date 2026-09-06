f708d567caa2ed2ee0eb9ad22965df1de1e5c3bc
Muhammad Keenan Basyir <mouchbobby@gmail.com>
2026-09-06 18:21:37 +0700

    Update bookings.js

--- service/src/store/bookings.js
+++ service/src/store/bookings.js
@@ -1,27 +1,35 @@
 // service/src/store/bookings.js
 
-// const db = require('../db'); // TODO: Import your database connection here later
+const pool = require('./db');
 
 async function checkIdempotencyKey(key) {
-    const query = `SELECT * FROM idempotency_keys WHERE key = $1`;
-    // return await db.query(query, [key]);
+    const result = await pool.query(
+        `SELECT * FROM idempotency_keys WHERE key = $1`,
+        [key]
+    );
+    return result.rows[0] || null;
 }
 
 async function saveIdempotencyKey(key, bodyHash, status, responseBody) {
-    const query = `
+    await pool.query(
+        `
         INSERT INTO idempotency_keys (key, body_hash, response_status, response_body)
         VALUES ($1, $2, $3, $4)
-    `;
-    // await db.query(query, [key, bodyHash, status, responseBody]);
+        `,
+        [key, bodyHash, status, responseBody]
+    );
 }
 
 async function createBooking(bookingId, courtId, startTime, endTime) {
-    const query = `
+    const result = await pool.query(
+        `
         INSERT INTO bookings (id, court_id, start_time, end_time, status)
         VALUES ($1, $2, $3, $4, 'confirmed')
-        RETURNING *;
-    `;
-    // return await db.query(query, [bookingId, courtId, startTime, endTime]);
+        RETURNING *
+        `,
+        [bookingId, courtId, startTime, endTime]
+    );
+    return result.rows[0];
 }
 
 module.exports = {
